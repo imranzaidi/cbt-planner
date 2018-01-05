d1c8a9e6c60ed3936594a1b251f771a1379cbb6e
Imran Ali Zaidi <imranalizaidi@yahoo.com>
2018-01-05 15:22:50 -0600

    refactor: Value model

--- app/models/elements/value.js
+++ app/models/elements/value.js
@@ -8,16 +8,18 @@ const mongoose = require('mongoose');
  *
  * Example usage:
  *
- * category (personal, interpersonal, health, professional, financial) - personal
+ * category - personal
  * valueStatement - I value integrity
  * description - Intellectual honesty is a function of critical thought and rational analysis
  */
+
+const CATEGORIES = ['personal', 'interpersonal', 'health and well-being', 'professional', 'financial'];
 
 const ValueSchema = new mongoose.Schema({
   timestamps: true,
   category: {
     type: String,
-    default: 'New category',
+    enum: CATEGORIES,
     trim: true,
     required: 'Category cannot be blank.'
   },
