69a7ba33e49f78c730854e6828e91ed5d8be35b4
Imran Ali Zaidi <imranalizaidi@yahoo.com>
2018-03-10 22:01:11 -0600

    Enums added and cleanup
    - added enums in costs folder
    - removed task and notes libraries / consts, no longer needed

---
+++ app/consts/enums.js
@@ -0,0 +1,10 @@
+const taskPriorities = ['a', 'b', 'c'],
+  taskStatuses = ['incomplete', 'in progress', 'completed', 'forwarded'],
+  valueCategories = ['personal', 'interpersonal', 'health and well-being', 'professional', 'financial'];
+
+
+module.exports = {
+  taskPriorities,
+  taskStatuses,
+  valueCategories
+};
